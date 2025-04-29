0d5f31d82ec1c9f986dd00735582a59bbd1b5af7
Ihor Oherchuk <igor.ogerchuk@gmail.com>
2025-04-30 01:45:46 +0200

    v-for

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const app = Vue.createApp({
       product: 'Socks',
       description: 'A pair of warm, fuzzy socks.',
       image: {src: './assets/images/socks_blue.jpg', alt: 'blue socks'},
+      inventory: 8,
+      isOnSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [{id: 1, color: 'blue'}, {id: 2, color: 'green'}]
     }
   }
 });
