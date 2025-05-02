5ab3a81dbb2762717fe5daa3e1b4d26f62e25efe
Ihor Oherchuk <igor.ogerchuk@gmail.com>
2025-05-02 19:39:35 +0200

    styles

--- main.js
+++ main.js
@@ -4,10 +4,27 @@ const app = Vue.createApp({
       product: 'Socks',
       description: 'A pair of warm, fuzzy socks.',
       image: {src: './assets/images/socks_blue.jpg', alt: 'blue socks'},
-      inventory: 8,
+      inventory: 0,
       isOnSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
-      variants: [{id: 1, color: 'blue'}, {id: 2, color: 'green'}]
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+      ],
+      cart:0,
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart++;
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart--;
+      }
+    },
+    updateImage(imageSrc) {
+      this.image.src = imageSrc;
     }
   }
 });
